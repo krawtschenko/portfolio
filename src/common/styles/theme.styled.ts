98a11154cf471400b024839210ec5e29c684b60a
Krawtschenko <eugenykravchenko@gmail.com>
2024-01-22 21:23:58 +0100

    Initial commit

---
+++ src/common/styles/theme.styled.ts
@@ -0,0 +1,39 @@
+export const theme = {
+	colors: {
+		primaryColor: 'hsl(165, 60%, 40%)',
+		titleColor: 'hsl(0, 0%, 100%)',
+		textColor: 'hsl(0, 0%, 92%)',
+		bgColor: 'hsl(216, 18%, 16%)',
+		bgColorAlt: 'hsl(213, 10%, 21%)',
+		containerColor: 'hsl(217, 18%, 14%)',
+		borderColor: 'hsl(210, 2%, 65%)'
+	},
+	gradients: {
+		firstGradient: 'linear-gradient(0deg, var(--bg-color-alt) 0%, var(--bg-color) 100%)',
+		secondGradient: 'linear-gradient(180deg, var(--bg-color-alt) 0%, var(--bg-color) 100%)',
+		thirdGradient: 'linear-gradient(180deg, var(--bg-color) 0%, var(--bg-color) 100%)'
+	},
+	fontsFamilies: {
+		bodyFont: "'Jost', sans-serif",
+		secondFont: "'Caveat', cursive",
+	},
+	fonts: {
+		biggestFontSize: '90px',
+		h1FontSize: '44px',
+		h2FontSize: '30px',
+		h3FontSize: '28px',
+		h4FontSize: '24px',
+		h5FontSize: '22px',
+		largestFontSize: '21px',
+		largerFontSize: '20px',
+		largeFontSize: '18px',
+		normalFontSize: '16px',
+		smallFontSize: '15px',
+		smallerFontSize: '14px',
+		tinyFontSize: '13px',
+	},
+	other: {
+		shadow: '5px 5px rgb(255 255 255 / 10%)',
+		transition: 'cubic-bezier(0.3, 0, 0.3, 1)'
+	}
+}
