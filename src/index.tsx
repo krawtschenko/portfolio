b9051727e1cebf02eb44c87260af5236b7e6c283
krawtschenko <eugenykravchenko@gmail.com>
2024-02-05 15:22:10 +0100

    fix css

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import {ThemeProvider} from "styled-components";
-import {theme} from "common/styles/theme.styled";
-import {GlobalStyles} from "common/styles/globalStyles";
+import App from 'app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
-	<ThemeProvider theme={theme}>
-		<App/>
-		<GlobalStyles/>
-	</ThemeProvider>,
-);
+root.render(<App/>);
