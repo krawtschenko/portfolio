ae059208498a8aca3269e772f3e67c3ebe83d5cc
Krawtschenko <eugenykravchenko@gmail.com>
2024-02-10 15:08:26 +0100

    Added form validation

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,8 +1,34 @@
 import style from "./contact.module.css";
 import reusable from "common/styles/reusable.module.css";
 import {FaRegAddressBook, FaRegEnvelope, FaRegUser, FaRegMap} from 'react-icons/fa'
+import * as yup from "yup";
+import {SubmitHandler, useForm} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+
+interface Inputs {
+	name: string
+	email: string
+	subject: string
+	message: string
+}
+
+const schema = yup.object({
+	name: yup.string().min(3).required(),
+	email: yup.string().email().required(),
+	subject: yup.string().min(3).required(),
+	message: yup.string().min(5).max(200).required(),
+});
 
 export const Contact = () => {
+	const {register, reset, handleSubmit, formState} = useForm<Inputs>({
+		mode: "onBlur",
+		resolver: yupResolver(schema),
+	})
+
+	const onSubmit: SubmitHandler<Inputs> = (data) => {
+		console.log(data)
+		reset()
+	}
 
 	return (
 		<section className={`${style.contact} ${reusable.section}`} id='contact'>
@@ -46,27 +72,42 @@ export const Contact = () => {
 					</div>
 				</div>
 
-				<form className={style.form}>
+				<form onSubmit={handleSubmit(onSubmit)} className={style.form}>
 					<div className={`${style.formGroup} ${reusable.grid}`}>
 						<div className={style.formDiv}>
 							<label className={`${style.formTag} ${reusable.text_cs}`}>Your full name <b>*</b></label>
-							<input type="text" className={style.formInput}/>
+							<input
+								type="text"
+								style={formState.errors.name ? {borderColor: '#d40000'} : {}}
+								className={style.formInput} {...register('name')}/>
+							{formState.errors.name && <span className={style.formError}>{formState.errors.name.message}</span>}
 						</div>
 
 						<div className={style.formDiv}>
 							<label className={`${style.formTag} ${reusable.text_cs}`}>Your Email Address <b>*</b></label>
-							<input type="email" className={style.formInput}/>
+							<input
+								type="email"
+								style={formState.errors.email ? {borderColor: '#d40000'} : {}}
+								className={style.formInput} {...register('email')}/>
+							{formState.errors.email && <span className={style.formError}>{formState.errors.email.message}</span>}
 						</div>
 					</div>
 
 					<div className={style.formDiv}>
 						<label className={`${style.formTag} ${reusable.text_cs}`}>Your Subject <b>*</b></label>
-						<input type="text" className={style.formInput}/>
+						<input
+							type="text"
+							style={formState.errors.subject ? {borderColor: '#d40000'} : {}}
+							className={style.formInput} {...register('subject')}/>
+						{formState.errors.subject && <span className={style.formError}>{formState.errors.subject.message}</span>}
 					</div>
 
 					<div className={`${style.formDiv} ${style.formArea}`}>
 						<label className={`${style.formTag} ${reusable.text_cs}`}>Your Message <b>*</b></label>
-						<textarea className={style.formInput}></textarea>
+						<textarea
+							style={formState.errors.message ? {borderColor: '#d40000'} : {}}
+							className={style.formInput} {...register('message')}></textarea>
+						{formState.errors.message && <span className={style.formError}>{formState.errors.message.message}</span>}
 					</div>
 
 					<div className={style.submit}>
